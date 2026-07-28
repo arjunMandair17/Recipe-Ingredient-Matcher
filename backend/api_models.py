f9c65731524156d1e9892d8aa0922358b55e2e6e
Arjun Mandair <arjunmandair@cmail.carleton.ca>
2026-07-28 13:53:28 -0400

    added database models

---
+++ backend/api_models.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel
+
+
+class IngredientResponse(BaseModel):
+    name: str
+    price: float
+    price_url: str
+    quantity: float
+    unit: str
+
+
+class RecipeRequest(BaseModel):
+    api_id: str
+    name: str
+    image_url: str
+    instructions: str
+
+class RecipeResponse(BaseModel):
+    id: int
+    name: str
+    image_url: str
+    instructions: str
+    ingredients: list[IngredientResponse]
